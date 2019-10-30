2fe6fa9efc96bdfe5c7ef30b794e85664ba5542a
Ricky Rojas <rickyrojas19@gmail.com>
2019-10-30 10:32:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,4 +22,4 @@ function unleashDog(dogName, dogBreed) {
   return `Unleash ${dogName} the ${dogBreed}`;
 }
 
-let routine = [wakeDog(dogName, dogBreed), leashDog(dogName, dogBreed), walkToPark(dogName, dogBreed), throwFrisbee(dogName, dogBreed), walkHome(dogName, dogBreed), unleashDog(dogName, dogBreed)]
+const routine = [wakeDog(dogName, dogBreed), leashDog(dogName, dogBreed), walkToPark(dogName, dogBreed), throwFrisbee(dogName, dogBreed), walkHome(dogName, dogBreed), unleashDog(dogName, dogBreed)]
