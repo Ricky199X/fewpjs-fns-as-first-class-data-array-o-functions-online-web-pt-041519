39de049fbf009f7b6a19ad0b2d14726267c0bafc
Ricky Rojas <rickyrojas19@gmail.com>
2019-10-30 10:26:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,7 @@ function throwFrisbee(dogName, dogBreed) {
 function walkHome(dogName, dogBreed) {
   return `Walk home with ${dogName} the ${dogBreed}`;
 }
+
+function unleashDog(dogName, dogBreed) {
+  return `Unleash ${dogName} the ${dogBreed}`;
+}
